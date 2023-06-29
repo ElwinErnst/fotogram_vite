bc1b0f32620cb00576e1298ba9390e5c4b714e37
elwinernst <elwin.ernst@hotmail.com>
2023-06-29 16:45:17 -0300

    arreglo likes

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 
-const LikeButton = ({ onLike }) => {
-  const [like, setLike] = useState(0);
+const LikeButton = ({ onLike, likes }) => {
+  const [like, setLike] = useState(likes);
 
   const addLike = () => {
     setLike(like + 1);
